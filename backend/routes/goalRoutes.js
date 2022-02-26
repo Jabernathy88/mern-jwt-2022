d7851d318ce6b7e45005b13d6ce40566d5519b83
Jeremy Abernathy <jea@JEREMYs-MacBook-Pro.local>
2022-02-26 13:53:03 -0500

    JSON response, start Routes folder.

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,8 +1,20 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/api/goals', (req, res) => {
-  res.status(200).json({message: 'GET goals route.'})
+router.get('/', (req, res) => {
+  res.status(200).json({message: 'GET: List goals route.'})
+})
+
+router.post('/', (req, res) => {
+  res.status(200).json({message: 'POST: Set a new goal route.'})
+})
+
+router.put('/', (req, res) => {
+  res.status(200).json({message: 'PUT: Update a goal route.'})
+})
+
+router.delete('/', (req, res) => {
+  res.status(200).json({message: 'DELETE: Remove a goal route.'})
 })
 
 module.exports = router
