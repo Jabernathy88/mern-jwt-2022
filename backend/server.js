330d1442a974d00196ecbe5acbc13b1f87711e1d
Jeremy Abernathy <jea@JEREMYs-MacBook-Pro.local>
2022-02-26 13:16:15 -0500

    Basic Node server.

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
-console.log("I am here.")
+const express = require('express')
+const dotenv = require('dotenv').config()
+const port = process.env.PORT || 3000
+
+const app = express()
+
+app.get('/api/goals', (req, res) => {
+  res.send('Get goals please.')
+})
+
+app.listen(port, () => console.log(`Node server started on port ${port}`))
