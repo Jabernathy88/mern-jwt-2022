419c3479cf0cc7e5e9f44e77c71314589393d86a
Jeremy Eugene Abernathy <jeabernath@printpack.com>
2022-03-08 15:00:37 -0500

    Partial finish Part I.

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,9 +1,12 @@
 const asyncHandler = require('express-async-handler')
 
+const Goal = require('../models/goalModel')
+
 // Get list all goals
 // route:   GET /api/goals
 // access   Private
 const getGoals = asyncHandler(async (req, res) => {
+  const goals = await Goal.find() // returns array of all records
   // console.log("Received GET request: api/ root")
   res.status(200).json({message: 'GET: List goals route.'})
 })
@@ -18,24 +21,46 @@ const setGoal = asyncHandler(async (req, res) => {
 
     throw new Error('Please add a text field.')
   }
+  const goal = await Goal.create({
+    text: req.body.text
+  })
+
   // console.log("Received POST request: api/ root")
-  res.status(200).json({message: 'POST: Set new goal route.'})
+  res.status(200).json(goal)
 })
 
 // Update a goal by id
 // route:   PUT /api/goals 
 // access   Private
 const updateGoal = asyncHandler(async (req, res) => {
+  const goal = await Goal.findById(req.params.id)
+
+  if(!goal) {
+    res.status(400)
+    throw new Error('Goal not found')
+  }
+  const updatedGoal = await Goal.findById(req.params.id, req.body, {
+    new: true, // seems imprecise, but covered by "not found" condition above? -JEA 
+  })
+
   // console.log("Received PUT request: api/ root")
-  res.status(200).json({message: `PUT: Update a goal, id: ${req.params.id}.`})
+  res.status(200).json({message: `PUT: Updated goal, object: ${updateGoal}.`})
 })
 
 // Delete a goal by id
 // route:   DELETE /api/goals
 // access   Private
 const deleteGoal = asyncHandler(async (req, res) => {
+  const goal = await Goal.findById(req.params.id)
+  if(!goal) {
+    res.status(400)
+    throw new Error('Goal not found')
+  }
+  await goal.remove
+
+
   // console.log("Received DELETE request: api/ root")
-  res.status(200).json({message: `DELETE: Remove a goal, id: ${req.params.id}.`})
+  res.status(200).json({message: `DELETE: Removed goal, id: ${req.params.id}.`})
 })
 
 module.exports = {
