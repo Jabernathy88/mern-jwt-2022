ab13a3f615903f60818f5a8ca78f9e058c09e791
Jeremy Eugene Abernathy <jeabernath@printpack.com>
2022-03-08 12:27:04 -0500

    Implement asyncHandler middleware.

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,15 +1,17 @@
+const asyncHandler = require('express-async-handler')
+
 // Get list all goals
 // route:   GET /api/goals
 // access   Private
-const getGoals = (req, res) => {
+const getGoals = asyncHandler(async (req, res) => {
   // console.log("Received GET request: api/ root")
   res.status(200).json({message: 'GET: List goals route.'})
-}
+})
 
 // Set new goal
 // route:   POST /api/goals 
 // access   Private
-const setGoal = (req, res) => {
+const setGoal = asyncHandler(async (req, res) => {
   if (!req.body.text) {
 
     res.status(400)
@@ -18,23 +20,23 @@ const setGoal = (req, res) => {
   }
   // console.log("Received POST request: api/ root")
   res.status(200).json({message: 'POST: Set new goal route.'})
-}
+})
 
 // Update a goal by id
 // route:   PUT /api/goals 
 // access   Private
-const updateGoal = (req, res) => {
+const updateGoal = asyncHandler(async (req, res) => {
   // console.log("Received PUT request: api/ root")
   res.status(200).json({message: `PUT: Update a goal, id: ${req.params.id}.`})
-}
+})
 
 // Delete a goal by id
 // route:   DELETE /api/goals
 // access   Private
-const deleteGoal = (req, res) => {
+const deleteGoal = asyncHandler(async (req, res) => {
   // console.log("Received DELETE request: api/ root")
   res.status(200).json({message: `DELETE: Remove a goal, id: ${req.params.id}.`})
-}
+})
 
 module.exports = {
   getGoals,
