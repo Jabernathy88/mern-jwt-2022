32619db35c5493f5c57b8ff6a17ecb1a778a2a52
Jeremy Eugene Abernathy <jeabernath@printpack.com>
2022-03-08 15:34:36 -0500

    Create userController, basic auth routes.

---
+++ backend/controllers/userController.js
@@ -0,0 +1,27 @@
+
+// Register new user
+// route:   POST /api/users
+// access   Public
+const registerUser = (req, res) => {
+  res.json({ message: 'Register User' })
+}
+
+// Authenticate user
+// route:   POST /api/users/login
+// access   Public
+const loginUser = (req, res) => {
+  res.json({ message: 'Login User' })
+}
+
+// Get current user info
+// route:   GET /api/users/me
+// access   Public
+const getMe = (req, res) => {
+  res.json({ message: 'User data display' })
+}
+
+module.exports = {
+  registerUser,
+  loginUser,
+  getMe
+}
