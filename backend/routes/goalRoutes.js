85560bc2cd3b4dc8e2263931ba508c9873e4cb01
Jeremy Eugene Abernathy <jeabernath@printpack.com>
2022-03-08 10:00:55 -0500

    Resume progress from Windows machine.

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -3,17 +3,18 @@ const router = express.Router()
 const { getGoals } = require('../controllers/goalController')
 
 router.get('/', (req, res) => {
+  console.log("Received GET request: api/ root")
   res.status(200).json({message: 'GET: List goals route.'})
 })
 
-router.post('/', getGoals)
+// router.post('/', getGoals)
 
 router.put('/', (req, res) => {
-  res.status(200).json({message: 'PUT: Update a goal route.'})
+  res.status(200).json({message: `PUT: Update a goal, id: ${req.params.id}.`})
 })
 
 router.delete('/', (req, res) => {
-  res.status(200).json({message: 'DELETE: Remove a goal route.'})
+  res.status(200).json({message: `DELETE: Remove a goal, id: ${req.params.id}.`})
 })
 
 module.exports = router
