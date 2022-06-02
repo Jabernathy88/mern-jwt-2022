d04c581ace4457b00d6672a33ea480b84974ace5
Jeremy Abernathy <jea@JEREMYs-MacBook-Pro.local>
2022-06-02 16:07:37 -0400

    Install and setup React Router for 3 pages.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,26 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Dashboard from './pages/Dashboard'
+import Login from './pages/Login'
+import Register from './pages/Register'
 
+// code was
+// <div className='container'>
+// <h2 className="placeholder">Mern App Practice...</h2>
+// </div>
 
 function App() {
   return (
-    <div className="">
-      <h2 className="placeholder">Mern App Practice</h2>
-
-    </div>
+    <>
+      <Router>
+        <div className='container'>
+          <Routes>
+            <Route path='/' element={<Dashboard />}></Route>
+            <Route path='/login' element={<Login />}></Route>
+            <Route path='/register' element={<Register />}></Route>
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
 }
 
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 function dashboard() {
   return (
     <div>dashboard</div>
