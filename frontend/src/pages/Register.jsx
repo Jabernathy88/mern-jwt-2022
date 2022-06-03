e22cea8343535d4a1b58cb1ee72fcef081f60b06
Jeremy Abernathy <jea@JEREMYs-MacBook-Pro.local>
2022-06-02 21:45:25 -0400

    Add Register New User form-group UI.

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,5 +1,5 @@
 import {useState, useEffect} from 'react'
-import FaUser from 'react-icons/fa'
+import { FaUser } from 'react-icons/fa'
 
 function Register() {
   const [formData, setFormData] = useState({
@@ -11,69 +11,79 @@ function Register() {
 
   const {name, email, password, password2} = formData
 
-  const onChange = () => {}
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }))
+  }
+
+  const onSubmit = (e) => {
+
+  }
 
   return (
     <>
       <section className="heading">
         <h1>
-          <faUser /> Register New User
+          <FaUser /> Register
         </h1>
         <p>
-          Please create an account.
+          Please create a new account.
         </p>
       </section>
       <section className="form">
-        <div className="form-group">
-          <input 
-            type="text" 
-            className="form-control" 
-            id='name' 
-            name='name'
-            value={name} 
-            placeholder='Enter your name' 
-            onChange={{onChange}}
-          />
-        </div>
-        <div className="form-group">
-          <input 
-            type="email"
-            className="form-control"
-            id='email'
-            name='email'
-            value={email} 
-            placeholder='Enter your email' 
-            onChange={{onChange}}
-          />
-        </div>
-        <div className="form-group">
-          <input 
-            type="password"
-            className="form-control" 
-            id='password'
-            name='password'
-            value={password} 
-            placeholder='Enter password' 
-            onChange={{onChange}}
-          />
-        </div>
-        <div className="form-group">
-          <input 
-            type="password"
-            className="form-control" 
-            id='password2'
-            name='password2'
-            value={password2} 
-            placeholder='Confirm password' 
-            onChange={{onChange}}
-          />
-        </div>
-        <div className="form-group">
-          <button type='submit' className="btn btn-block">
-            Submit
-          </button>
-        </div>
-
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <input 
+              type="text" 
+              className="form-control" 
+              id='name' 
+              name='name'
+              value={name} 
+              placeholder='Enter your name' 
+              onChange={{onChange}}
+            />
+          </div>
+          <div className="form-group">
+            <input 
+              type="email"
+              className="form-control"
+              id='email'
+              name='email'
+              value={email} 
+              placeholder='Enter your email' 
+              onChange={{onChange}}
+            />
+          </div>
+          <div className="form-group">
+            <input 
+              type="password"
+              className="form-control" 
+              id='password'
+              name='password'
+              value={password} 
+              placeholder='Enter password' 
+              onChange={{onChange}}
+            />
+          </div>
+          <div className="form-group">
+            <input 
+              type="password"
+              className="form-control" 
+              id='password2'
+              name='password2'
+              value={password2} 
+              placeholder='Confirm password' 
+              onChange={{onChange}}
+            />
+          </div>
+          <div className="form-group">
+            <button type='submit' className="btn btn-block">
+              Submit
+            </button>
+          </div>
+        </form>
       </section>
     </>
   )
