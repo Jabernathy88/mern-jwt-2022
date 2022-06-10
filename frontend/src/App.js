09695fcc978cb2a427c9353631a09f070fc81216
Jeremy Abernathy <jea@JEREMYs-MacBook-Pro.local>
2022-06-09 21:33:22 -0400

    Update frontend App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,10 @@
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import Header from './components/Header'
 import Dashboard from './pages/Dashboard'
 import Login from './pages/Login'
 import Register from './pages/Register'
-
-// code was
-// <div className='container'>
-// <h2 className="placeholder">Mern App Practice...</h2>
-// </div>
 
 function App() {
   return (
@@ -16,14 +13,15 @@ function App() {
         <div className='container'>
           <Header />
           <Routes>
-            <Route path='/' element={<Dashboard />}></Route>
-            <Route path='/login' element={<Login />}></Route>
-            <Route path='/register' element={<Register />}></Route>
+            <Route path='/' element={<Dashboard />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
           </Routes>
         </div>
       </Router>
+      <ToastContainer />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
